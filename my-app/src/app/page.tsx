cb5c4d101b8a0d53028c8a5569f10ba521f9f473
XxKillerDemonxX <149117791+XxKillerDemonxX@users.noreply.github.com>
2024-04-26 21:24:37 -0700

    first commit

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,11 +1,14 @@
 import { askBot } from "../data";
-
+import Link from "next/link";
 export default async function main() {
   const res = await askBot("Reply with emotions", "How are you feeling?");
+  
 
   return (
     <div className="w-screen h-screen flex items-center justify-center p-10">
-        {res}
+      <Link href = "/login">
+        Click to Login
+      </Link>
     </div>
   )
 }
