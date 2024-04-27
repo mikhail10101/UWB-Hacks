a45336c70ea230edbbbee150a0693d085617d8c5
mikhail10101 <mikhail.borbe.lg@gmail.com>
2024-04-27 14:03:51 -0700

    Show session

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,7 +1,15 @@
+import { getServerSession } from "next-auth"
+import { authOptions } from "./api/auth/[...nextauth]/auth-options"
+
 export default async function main() {
+  const session = await getServerSession(authOptions)
+  
   return (
     <div className="w-screen h-screen flex items-center justify-center p-10">
         Home
+        {
+          session ? <p>In session</p> : <p>No session</p>
+        }
     </div>
   )
 }
