7914906cf9d66ca081dc86d32f8828e2ed75f597
ImVarity <JoshuaLazarte2004@gmail.com>
2024-04-27 14:30:46 -0700

    homepage
    more

--- my-app/src/app/components/hambutton.tsx
+++ my-app/src/app/components/hambutton.tsx
@@ -11,14 +11,17 @@ const HamButton = () => {
     };
   
     return (
-      <div className={`fixed top-0 left-[10rem] h-full w-64 bg-white z-10 transform transition-transform ${isOpen ? 'translate-x-[6rem]' : '-translate-x-[10rem]'}`}>
-        <button onClick={toggleSidebar} className="absolute top-[2rem] left-[2rem]">
-          <svg className="h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-          </svg>
-        </button>
-        <div className="p-4">Sidebar Content</div>
+      <>
+      <button onClick={toggleSidebar} className="absolute top-[1.25rem] left-[1.5rem] z-10">
+      <svg className="h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+      </svg>
+      </button>
+      <div className={`fixed top-0 left-[-8rem] h-full w-[16rem] bg-white transform transition-transform ${isOpen ? 'translate-x-[6rem]' : '-translate-x-[10rem]'}`}>
+
+        <div className="p-4"></div>
       </div>
+      </>
     );
 }
 
