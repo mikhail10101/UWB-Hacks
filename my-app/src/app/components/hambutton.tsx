d459d31afc9aba52513aae1fc3eadeaee22ebe29
ImVarity <JoshuaLazarte2004@gmail.com>
2024-04-27 23:51:57 -0700

    last page

--- my-app/src/app/components/hambutton.tsx
+++ my-app/src/app/components/hambutton.tsx
@@ -16,12 +16,12 @@ const HamButton = ({ display }: {
   
     return (
       <>
-      <button onClick={toggleSidebar} className="absolute top-[1.25rem] left-[1.5rem] z-10">
+      <button onClick={toggleSidebar} className="absolute top-[1.25rem] left-[1.5rem] z-50">
       <svg className="h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
       </svg>
       </button>
-        <div className={`bg-white fixed flex justify-center top-0 left-[-8rem] h-full w-[16rem] bg-white transform transition-transform ${isOpen ? 'translate-x-[8rem]' : '-translate-x-[10rem]'}`}>
+        <div className={`bg-white fixed flex justify-center top-0 left-[-8rem] h-full w-[16rem] transform transition-transform ${isOpen ? 'translate-x-[8rem]' : '-translate-x-[10rem]'}`}>
           <div className='flex flex-col text-black mt-[5rem] w-[11rem]'>
             {
               display.map((d, index) => {
